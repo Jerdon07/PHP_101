570633cf1bc092b62e8713d7110088c195caa781
Jerdon07 <jdlitaoen@gmail.com>
2025-08-02 18:12:28 +0800

    Conditionals and Booleans

--- index.php
+++ index.php
@@ -4,13 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
+    <style>
+        body {
+            place-items: center;
+            display: grid;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1>
         <?php
-            $greeting = "Hello";
-            echo "$greeting Everyone";
+            $name = "Dark Matter.";
+            $read = false
         ?>
+
+    <h1>
+        <?php if($read) :?>
+            You have read <?= $name ?>
+        <?php else :?>
+            You have NOT read <?= $name ?>
+        <?php endif ?>
     </h1>
 </body>
 </html>
