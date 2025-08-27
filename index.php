4acdd4c5549884c9977455202c1a8d411a1b3b64
Jerdon07 <jdlitaoen@gmail.com>
2025-08-27 17:05:54 +0800

    Make a PHP Router

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
 <?php
 
-require 'functions.php';
-
-$heading = "Home";
-
-// echo "test";
-// echo ["test1", "test2"];
-// var_dump(["test1", "test2"]);
-
-// echo "<pre>";
-// var_dump($_SERVER);
-// echo "</pre>";
-// die();
-
-function dd($value) {
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
-
-    die();
-}
-
-// dd($_SERVER);
-// dd($heading);
-
-// echo $_SERVER['REQUEST_URI'];
-
-require 'views/index.view.php';
+echo 'hello';
