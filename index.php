11909e1391ad72a952050b1ab36ecdb30bf18fbb
Jerdon07 <jdlitaoen@gmail.com>
2025-09-19 22:29:43 +0800

    PDO First Steps

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
 require 'functions.php';
+// require 'router.php';
 
-require 'router.php';
+// Connect to our MySQL Database.
+//PHP Data Objects (PDO) is a database access layer providing a uniform method of access to multiple databases.
+
+class Person{
+    public $name;
+    public $age;
+
+    public function breathe() {
+        echo $this->name . " is breathing. ";
+        echo "<br> Inhale... Exhale...";
+    }
+}
+
+$person = new Person();
+
+$person->name = "John";
+$person->age = 30;
+$person->breathe();
