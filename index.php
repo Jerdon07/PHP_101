eb5d505aa3aa7c3c74f648e452988229eef58d0d
Jerdon07 <jdlitaoen@gmail.com>
2025-09-30 19:53:58 +0800

    Extract a PHP Database Class

--- index.php
+++ index.php
@@ -1,22 +1,8 @@
 <?php
 
 require 'functions.php';
-// require 'router.php';
+require 'Database.php';
 
-// Connect to our MySQL Database.
-//PHP Data Objects (PDO) is a database access layer providing a uniform method of access to multiple databases.
-
-//DSN Data Source Name
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8";
-
-$pdo = new PDO($dsn, "root");
-
-$statement = $pdo -> prepare("SELECT * FROM `post`");
-
-$statement -> execute();
-
-$posts = $statement -> fetchAll(PDO::FETCH_ASSOC);
-
-foreach($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+$db = new Database();
+$posts = $db -> query("SELECT * FROM post")->fetchAll(PDO::FETCH_ASSOC);
+dd($posts);
